f602be48a4454ecc0425acb62e064f31235f1d03
Ethan Delage <edelage@z2r8p4.42lyon.fr>
2023-01-16 10:42:54 +0100

    Refactor unset builtin

--- include/builtin.h
+++ include/builtin.h
@@ -1,32 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edelage <edelage@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/10 11:18:00 by edelage           #+#    #+#             */
-/*   Updated: 2023/01/10 11:18:00 by edelage          ###   ########lyon.fr   */
+/*   Created: 2023/01/16 10:09:00 by edelage           #+#    #+#             */
+/*   Updated: 2023/01/16 10:09:00 by edelage          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <readline/readline.h>
+#ifndef BUILTIN_H
+# define BUILTIN_H
+
 #include "envp.h"
 
-int	main(int argc, char **argv, char **envp)
-{
-	t_hashtable	*envp_dict;
-	char		*line;
+/* ----------	HASHTABLE FUNCTIONS	---------- */
 
-	(void) argc;
-	(void) argv;
-	envp_dict = envp_to_dict(envp);
-	if (errno)
-		return (errno);
-	hashtable_display(envp_dict);
-	hashtable_clear(envp_dict);
-	line = readline(">");
-	printf("%s", line);
-	free(line);
-	return (0);
-}
+int	builtin_unset(t_hashtable *envp_dict, const char *name);
+
+#endif
--- src/exec/builtin/unset/unset.c
+++ src/exec/builtin/unset/unset.c
@@ -9,9 +9,9 @@
 /*   Updated: 2023/01/14 18:17:00 by edelage          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
-#include "envp.h"
+#include "builtin.h"
 
-int	unset(t_hashtable *envp_dict, const char *name)
+int	builtin_unset(t_hashtable *envp_dict, const char *name)
 {
 	return (hashtable_remove_one(envp_dict, name));
 }
