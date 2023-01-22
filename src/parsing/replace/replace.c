28e3e475d7827456b27be28143d1af9680235c7a
edelage <ethandelage04@gmail.com>
2023-01-22 23:46:53 +0100

    Norm and optimise replace

--- src/parsing/replace/replace.c
+++ src/parsing/replace/replace.c
@@ -12,9 +12,12 @@
 #include <stdlib.h>
 #include "envp.h"
 
+static char	*check_env_var(t_hashtable *envp_dict, char *line, size_t *index,
+				char *quote);
 static void	change_quote_value(char *quote, char c);
-static char	*replace_env_var(t_hashtable *envp_dict, char *line, size_t index);
-static char	*add_env_var(char *line, char *value, size_t index, size_t end_index);
+static char	*replace_env_var(t_hashtable *envp_dict, char *line, size_t *index);
+static char	*add_env_var(char *line, char *value, size_t *index,
+				size_t end_index);
 
 int	valid_char(char c)
 {
@@ -32,19 +35,9 @@ char	*replace(t_hashtable *envp_dict, char *line)
 	{
 		if (line[index] == '$' && quote != '\'')
 		{
-			if (quote == '"' && !valid_char(line[index + 1]))
-			{
-				index++;
-				break ;
-			}
-			line = replace_env_var(envp_dict, line, index + 1);
+			line = check_env_var(envp_dict, line, &index, &quote);
 			if (errno)
-			{
-				free(line);
 				return (NULL);
-			}
-			quote = 0;
-			index = 0;
 		}
 		else
 		{
@@ -56,12 +49,28 @@ char	*replace(t_hashtable *envp_dict, char *line)
 	return (line);
 }
 
+static char	*check_env_var(t_hashtable *envp_dict, char *line, size_t *index,
+				char *quote)
+{
+	if (*quote == '"' && !valid_char(line[*index + 1]))
+		(*index)++;
+	else
+	{
+		(*index)++;
+		line = replace_env_var(envp_dict, line, index);
+		if (errno)
+		{
+			free(line);
+			return (NULL);
+		}
+	}
+	return (line);
+}
+
 static void	change_quote_value(char *quote, char c)
 {
 	if (c == '"')
 	{
-		if (*quote == '\'')
-			return ;
 		if (*quote == '"')
 			*quote = 0;
 		else if (*quote == 0)
@@ -69,37 +78,41 @@ static void	change_quote_value(char *quote, char c)
 	}
 	else
 	{
-		if (*quote == '"')
-			return ;
-		else if (*quote == '\'')
+		if (*quote == '\'')
 			*quote = 0;
 		else if (*quote == 0)
 			*quote = '\'';
 	}
 }
 
-static char	*replace_env_var(t_hashtable *envp_dict, char *line, size_t index)
+static char	*replace_env_var(t_hashtable *envp_dict, char *line, size_t *index)
 {
 	t_dict	*env;
 	char	*name;
 	size_t	end_index;
 
-	end_index = index;
+	end_index = *index;
 	while (line[end_index] && !ft_isspace(line[end_index])
-		&& line[end_index] != '"' && line[end_index] != '\'' && line[end_index] != '$')
+		&& line[end_index] != '"' && line[end_index] != '\''
+		&& line[end_index] != '$')
 		end_index++;
-	name = ft_substr(line, index, end_index - index);
+	name = ft_substr(line, *index, end_index - *index);
 	if (errno)
+	{
+		free(line);
 		return (NULL);
+	}
 	env = hashtable_search(envp_dict, name);
 	if (env == NULL)
 		line = add_env_var(line, "\0", index, end_index);
 	else
 		line = add_env_var(line, env->value, index, end_index);
+	free(name);
 	return (line);
 }
 
-static char	*add_env_var(char *line, char *value, size_t index, size_t end_index)
+static char	*add_env_var(char *line, char *value, size_t *index,
+				size_t end_index)
 {
 	const size_t	len_value = ft_strlen(value);
 	const size_t	len_line = ft_strlen(line);
@@ -107,13 +120,18 @@ static char	*add_env_var(char *line, char *value, size_t index, size_t end_index
 	char			*start;
 	char			*new_line;
 
-	new_line = (char *) malloc(sizeof(char) * (len_line + index - end_index + len_value));
+	new_line = (char *) malloc(sizeof(char)
+			* (len_line + *index - end_index + len_value));
 	if (errno)
+	{
+		free(line);
 		return (NULL);
-	start = ft_substr(line, 0, index - 1);
+	}
+	start = ft_substr(line, 0, *index - 1);
 	end = ft_substr(line, end_index, len_line - end_index);
 	new_line = ft_strjoin(start, value);
 	new_line = ft_strjoin(new_line, end);
+	*index = *index + len_value - 1;
 	free(line);
 	return (new_line);
 }
