e2129d2d25388dcd9f2e2e0f1478a227201f626f
Hugo Ferraud <ferraudhugo@gmail.com>
2023-01-23 13:07:56 +0100

    add fork in execve

--- src/exec/router/router.c
+++ src/exec/router/router.c
@@ -30,9 +30,7 @@ int	cmd_router(t_cmd_token *cmd_token, t_hashtable *envp_dict)
 	else if (ft_strcmp(cmd_token->head, "export") == 0)
 		return (builtin_export(envp_dict, args));
 	else
-	{
 		return (exec_bin(cmd_token, envp_dict));
-	}
 }
 
 static int	exec_bin(t_cmd_token *cmd_token, t_hashtable *envp_dict)
@@ -45,13 +43,17 @@ static int	exec_bin(t_cmd_token *cmd_token, t_hashtable *envp_dict)
 
 	args = (char **) cmd_token->body;
 	cmd_path = cmd_find_path(cmd_token, envp_dict);
+	if (cmd_path == NULL)
+	{
+		ft_putstr_fd("minishell: commandd not found: ", STDERR_FILENO);
+		ft_putendl_fd(cmd_token->head, STDERR_FILENO);
+		return (127);
+	}
 	envp = hashtable_get_array(envp_dict, 0);
 	pid = fork();
 	return_value = 0;
 	if (pid == 0)
-	{
 		execve(cmd_path, args, envp);
-	}
 	else
 	{
 		waitpid(pid, &return_value, 0);
