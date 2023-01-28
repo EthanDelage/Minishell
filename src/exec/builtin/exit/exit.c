38695c9e2f58f0f362ff56702bbe9094144f4ac1
mururoahh <ferraudhugo@gmail.com>
2023-01-28 18:09:40 +0100

    edit exit_builtin

--- src/exec/builtin/exit/exit.c
+++ src/exec/builtin/exit/exit.c
@@ -11,9 +11,11 @@
 /* ************************************************************************** */
 #include "builtin.h"
 
+static int	is_numeric_arg(char *arg);
+
 int	builtin_exit(t_hashtable *envp_dict, t_token *token_stack, char **args)
 {
-	if (args[1] != NULL)
+	if (args[1] && args[2] != NULL)
 	{
 		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 		return (1);
@@ -22,5 +24,23 @@ int	builtin_exit(t_hashtable *envp_dict, t_token *token_stack, char **args)
 		hashtable_clear(envp_dict);
 	if (token_stack)
 		token_clear(&token_stack);
-	//TODO add exit value and error handling when arg is non numeric
+	printf("exit\n");
+	if (args[1])
+	{
+		if (!is_numeric_arg(args[1]))
+			ft_putstr_fd("minishell: exit: numeric argument required\n", 2);
+		exit(ft_atoi(args[1]));
+	}
+	exit(0);
+}
+
+static int	is_numeric_arg(char *arg)
+{
+	while (*arg)
+	{
+		if (!ft_isdigit(*arg))
+			return (0);
+		arg++;
+	}
+	return (1);
 }
