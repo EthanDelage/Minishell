f3ccb12db2575b58f7ff236f80cb79dafeab7cfc
Ethan Delage <edelage@z3r7p3.42lyon.fr>
2023-02-07 11:31:26 +0100

    Norm wildcard

--- src/parsing/replace/wildcards/wildcards.c
+++ src/parsing/replace/wildcards/wildcards.c
@@ -13,38 +13,50 @@
 
 extern unsigned char	g_return_value;
 
+static char	*wildcard_get_file(char *template, DIR *dir);
 static bool	wildcard_is_valid(char *filename, char *template);
 
 char	*wildcard_replace(char *template)
 {
 	DIR				*dir;
-	struct dirent	*entry;
 	char			*result;
 
 	result = NULL;
 	dir = opendir(".");
 	if (errno)
 		return (error(template, result));
-	entry = readdir(dir);
+	result = wildcard_get_file(template, dir);
 	if (errno)
 		return (error(template, result));
+	if (closedir(dir) == -1)
+		return (error(template, result));
+	if (result == NULL)
+		return (template);
+	free(template);
+	return (result);
+}
+
+static char	*wildcard_get_file(char *template, DIR *dir)
+{
+	struct dirent	*entry;
+	char			*result;
+
+	result = NULL;
+	entry = readdir(dir);
+	if (errno)
+		return (NULL);
 	while (entry)
 	{
 		if (wildcard_is_valid(entry->d_name, template))
 		{
 			result = strjoin_space(result, entry->d_name);
 			if (errno)
-				return (error(template, result));
+				return (NULL);
 		}
 		entry = readdir(dir);
 		if (errno)
-			return (error(template, result));
+			return (NULL);
 	}
-	if (closedir(dir) == -1)
-		return (error(template, result));
-	if (result == NULL)
-		return (template);
-	free(template);
 	return (result);
 }
 
